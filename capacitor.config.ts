993ccd5af0039eb65198dacd6afc30b3e61e8c76
luisbytes <luisbytesdev@gmail.com>
2025-04-18 16:14:38 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edge-to-edge',
+  webDir: 'www'
+};
+
+export default config;
